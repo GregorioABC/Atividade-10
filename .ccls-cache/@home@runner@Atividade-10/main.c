5f1b1a8a319c6d91d987676575615812c89e7c51
GregorioABC <87394080+GregorioABC@users.noreply.github.com>
2023-09-14 11:51:27 +0000

    questão 3 completa

--- .ccls-cache/@home@runner@Atividade-10/main.c
+++ .ccls-cache/@home@runner@Atividade-10/main.c
@@ -39,15 +39,27 @@
 //   return 0;
 // }
 //3
-#include <stdlib.h>
-#include <stdio.h>
+// #include <stdlib.h>
+// #include <stdio.h>
 
-int main (void){
-  int vetor [10];
-  int maior;
-  for (int i= 0;i<10; i++){
-    printf("informe o vetor %d",i);
-    scanf("%d",&vetor[i]);
-  }
-}
+// int main (void){
+//   int vetor [10];
+//   int maior;
+//   int posicao;
+//   for (int i= 0;i<10; i++){
+//     printf("informe o vetor %d ",i);
+//     scanf("%d",&vetor[i]);
+//   }
+//   maior= vetor[0];
+//   posicao=0;
+//   for(int i=0;i<10;i++){
+//     if(vetor[i]>maior){
+//       maior=vetor[i];
+//       posicao = i;
+//     }
+//   }
+//   printf("O menor vetor é vetor %d",maior);
+//   printf(" e se encontra na posição %d\n",posicao);
+//   return 0;
+// }
 
