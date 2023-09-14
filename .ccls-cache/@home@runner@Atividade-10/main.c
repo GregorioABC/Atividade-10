264bfb88fda8c2b047859b9b3a5abb69966bf067
GregorioABC <87394080+GregorioABC@users.noreply.github.com>
2023-09-14 11:36:10 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Atividade-10/main.c
@@ -0,0 +1,21 @@
+//aluno Gregório de Albuquerque Borba Cavalcanti
+//1
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(void) {
+//   int vetor [10];
+//   int maior;
+//   for (int i= 0;i<10; i++){
+//     printf("informe o vetor %d",i);
+//     scanf("%d",&vetor[i]);
+//   }
+//   maior = vetor[0];
+//   for(int i=0;i<10;i++){
+//     if(vetor[i]>maior){
+//       maior=vetor[i];
+//     }
+//   }
+//   printf("O maior vetor é vetor %d",maior);
+//   return 0;
+// }
+//2
