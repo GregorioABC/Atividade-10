a335f26f501053189d8dc4cc39bddd582112bd0e
GregorioABC <87394080+GregorioABC@users.noreply.github.com>
2023-09-14 22:59:02 +0000

    todas as respostas

--- main.c
+++ main.c
@@ -41,7 +41,6 @@
 //3
 // #include <stdlib.h>
 // #include <stdio.h>
-
 // int main (void){
 //   int vetor [10];
 //   int maior;
@@ -62,4 +61,57 @@
 //   printf(" e se encontra na posição %d\n",posicao);
 //   return 0;
 // }
-
+//4
+// #include <stdlib.h>
+// #include <stdio.h>
+// int main (void){
+//   int valores[5];
+//     int maior, menor;
+//     int posicao_maior, posicao_menor;
+//     printf("Digite 5 valores:\n");
+//     for (int i = 0; i < 5; i++) {
+//         printf("Digite o %dº valor: ", i + 1);
+//         scanf("%d", &valores[i]);
+//     }
+//     maior = valores[0];
+//     menor = valores[0];
+//     posicao_maior = 0;
+//     posicao_menor = 0;
+//     for (int i = 1; i < 5; i++) {
+//         if (valores[i] > maior) {
+//             maior = valores[i];
+//             posicao_maior = i;
+//         }
+//         if (valores[i] < menor) {
+//             menor = valores[i];
+//             posicao_menor = i;
+//         }
+//     }
+//     printf("O maior valor %d . Na posição %d.", maior, posicao_maior);
+//     printf("O menor valor %d . Na posição %d.", menor, posicao_menor);
+//     return 0;
+// }
+//5
+// #include <stdlib.h>
+// #include <stdio.h>
+// int main (void){
+//   int vetor[10];
+//     int valoresIguais = 0;
+//     printf("Digite 10 valores inteiros:\n");
+//     for (int i = 0; i < 10; i++) {
+//         printf("Digite o %dº valor: ", i + 1);
+//         scanf("%d", &vetor[i]);
+//     }
+//     for (int i = 0; i < 9; i++) {
+//         for (int j = i + 1; j < 10; j++) {
+//             if (vetor[i] == vetor[j]) {
+//                 valoresIguais = 1; 
+//                 printf("Valores iguais encontrados: %d e %d", vetor[i], vetor[j]);
+//             }
+//         }
+//     }
+//     if (valoresIguais == 0) {
+//         printf("Não foram encontrados valores iguais no vetor.");
+//     }
+//     return 0;
+// }
