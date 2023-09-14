95a04a0ac614ea4a7e25b8e5715b1c8a1e019ac9
GregorioABC <87394080+GregorioABC@users.noreply.github.com>
2023-09-14 11:45:00 +0000

    Até a 3 questão e redigindo as questões

--- .ccls-cache/@home@runner@Atividade-10/main.c
+++ .ccls-cache/@home@runner@Atividade-10/main.c
@@ -1,4 +1,5 @@
 //aluno Gregório de Albuquerque Borba Cavalcanti
+//atividade busca em vetor
 //1
 // #include <stdio.h>
 // #include <stdlib.h>
@@ -19,3 +20,34 @@
 //   return 0;
 // }
 //2
+// #include <stdio.h>
+// #include<stdlib.h>
+// int main (void){
+//   int vetor [10];
+//   int menor;
+//   for (int i= 0;i<10; i++){
+//     printf("informe o vetor %d",i);
+//     scanf("%d",&vetor[i]);
+//   }
+//   maior = vetor[0];
+//   for(int i=0;i<10;i++){
+//     if(vetor[i]<menor){
+//       menor=vetor[i];
+//     }
+//   }
+//   printf("O menor vetor é vetor %d",menor);
+//   return 0;
+// }
+//3
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void){
+  int vetor [10];
+  int maior;
+  for (int i= 0;i<10; i++){
+    printf("informe o vetor %d",i);
+    scanf("%d",&vetor[i]);
+  }
+}
+
